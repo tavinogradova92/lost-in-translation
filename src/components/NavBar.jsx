3cf25cb5d4b0fecec139be5429e1b4d3258247c4
tavinogradova92 <tavinogradova92@gmail.com>
2020-11-19 14:37:00 +0100

    Profile element of a navbar appears when user logs in

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,29 @@
+/* eslint-disable no-undef */
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 
 function Navbar() {
+  
+  const history = useHistory();
+
+  const username = localStorage.getItem('userName');
+
+  function handleSuccessfulLogin() {
+    history.push('/profile');
+  }
+
   return (
-    <div className="container">
-      <h2>Lost in Translation</h2>
+    <div className="navBar">
+      Lost In Translation
+      {username !== null &&
+        <div className="container">
+          <img src="https://www.pngitem.com/pimgs/m/87-877270_logo-icon-profile-png-transparent-png.png" alt="profile"/>
+          <button onClick={handleSuccessfulLogin}>{username}</button>
+        </div>
+      }
     </div>
   );
 }
+
 
 export default Navbar;
