c3558200bde7f472c0ce705fe583b515faa81373
tavinogradova92 <tavinogradova92@gmail.com>
2020-11-20 14:18:06 +0100

    Navbars part with user name and link to profile appears only after successful login

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,24 +1,24 @@
-/* eslint-disable no-undef */
-import React from 'react';
+import React, { useContext }  from 'react';
 import { useHistory } from 'react-router-dom';
+import { AppContext } from '../state/AppContext';
 
 function Navbar() {
+
+  const { username, isLoggedIn } = useContext(AppContext);
   
   const history = useHistory();
 
-  const username = localStorage.getItem('userName');
-
-  function handleSuccessfulLogin() {
+  function redirectToProfile() {
     history.push('/profile');
   }
 
   return (
     <div className="navBar">
       Lost In Translation
-      {username !== null &&
+      {username !== null && isLoggedIn === true &&
         <div className="container">
           <img src="https://www.pngitem.com/pimgs/m/87-877270_logo-icon-profile-png-transparent-png.png" alt="profile"/>
-          <button onClick={handleSuccessfulLogin}>{username}</button>
+          <button onClick={redirectToProfile}>{username}</button>
         </div>
       }
     </div>
