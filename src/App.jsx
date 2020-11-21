954240ed51b6e206305548da4c513e6fa95292bd
tavinogradova92 <tavinogradova92@gmail.com>
2020-11-21 19:37:35 +0100

    Conditional router added

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import NavBar from './components/NavBar';
 import Translation from './components/Translation';
 import Profile from './components/Profile';
 import { AppContext } from './state/AppContext';
+import NotFound from './components/NotFound';
 
 function App() {
 
@@ -19,26 +20,29 @@ function App() {
   }
   }, [])
 
-  useEffect(() => {
-    const savedLoginStatus = localStorage.getItem("loginStatus");
-
-    if(savedLoginStatus) {
-      changeLoginStatus(savedLoginStatus);
-  }
-  }, [])
+  const savedLoginStatus = localStorage.getItem("loginStatus");
 
   return (
     <BrowserRouter>
       <NavBar/>
       <main className="app">
-        <Switch>
+        {savedLoginStatus !== 'true' && <Switch>
           <Route path="/login" component={Homepage}/>
-          <Route path="/translation" component={Translation}/>
-          <Route path="/profile" component={Profile}/>
-          <Route path="/">
+          <Route path="*">
               <Redirect to="/login"/>
           </Route>
-        </Switch>
+        </Switch>}
+        {savedLoginStatus === 'true' && <Switch>
+          <Route exact path="/login">
+            <Redirect to ="/translation"/>
+          </Route>
+          <Route exact path="/translation" component={Translation}/>
+          <Route exact path="/profile" component={Profile}/>
+          <Route exact path="/">
+              <Redirect to="/translation"/>
+          </Route>
+          <Route path="*" component={NotFound} />
+        </Switch>}
       </main>
     </BrowserRouter>
       
